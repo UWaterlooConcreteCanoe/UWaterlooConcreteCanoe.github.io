83577ce895364d5a79ea296fad3c6b04581e211a
Padena Rasouli-Baghban <padenarasouli@gmail.com>
2023-05-01 23:14:55 -0400

    fix join form

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -7,7 +7,7 @@ function Join() {
         <div className="About" style={{marginTop: "75px"}}>
             <div className="row justify-content-center text-center" style={{marginTop: "75px", marginBottom: "1.5%"}}><h1>Join Us</h1></div>
             <div className="row justify-content-center text-center" style={{height:"100%"}}>
-                <iframe width="640px" height="600px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=h1o6cprzIkqSRz_CQMATlnO8OjhqoIRIvJhMsNGBHK5UMDlIU0VNVTdMSTVMTTlCRThNNlo4R0FISy4u&embed=true" style={{border: "none", maxWidth: "100%", maxHeight: "100vh"}} > </iframe>
+                <iframe width="640px" height="600px" title="join-form" src="https://forms.office.com/Pages/ResponsePage.aspx?id=h1o6cprzIkqSRz_CQMATlnO8OjhqoIRIvJhMsNGBHK5UMDlIU0VNVTdMSTVMTTlCRThNNlo4R0FISy4u&embed=true" style={{border: "none", maxWidth: "100%", maxHeight: "100vh"}}> </iframe>
             </div>
         </div>
     );
