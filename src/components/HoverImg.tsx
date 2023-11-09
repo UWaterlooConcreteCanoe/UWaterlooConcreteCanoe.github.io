174626cf11062020f799333fe261cfc3f842f2cd
Kiran S. Prasad <krn7prsd@gmail.com>
2023-11-08 23:43:03 -0500

    HoverImg component created

---
+++ src/components/HoverImg.tsx
@@ -0,0 +1,17 @@
+import { hover } from "@testing-library/user-event/dist/hover";
+import React from "react";
+import "./HoverImg.css"
+
+function HoverImg(props: {src: string, hoverText: string, width: string, href: string}) {
+
+    return (
+        <a href={props.href}>
+            <div className="img_wrap">
+                <img className="img_img" src={props.src} width={props.width} />
+                <p className="img_description">{props.hoverText}</p>
+            </div>
+        </a>
+    );
+}
+
+export default HoverImg;
