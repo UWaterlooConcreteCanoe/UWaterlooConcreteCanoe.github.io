c95103064664ede9e0030d5274f17f87ea540201
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-07-24 22:31:08 -0400

    add alt text for logo

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
     <div className="App" style={{overflowX: "hidden" }}>
       <header className="App-header">
         <nav className='navbar fixed-top navbar-expand-lg navbar-dark bg-dark'>
-          <Link to={'/'} className='navbar-brand ms-3'><img src={Logo} width="54" height="44" /></Link>
+          <Link to={'/'} className='navbar-brand ms-3'><img src={Logo} alt="uWaterloo Concrete Canoe Logo" width="54" height="44" /></Link>
           <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown' aria-controls='navbarNavDropdown' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
