a8f724039a92edb0a236b9b10837a69ad378b813
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-07-24 22:28:18 -0400

    update logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Link, Outlet } from 'react-router-dom';
+import Logo from "./assets/ConcreteCanoeLogoImage.png";
 
 function Footer() {
   return (
@@ -34,7 +35,7 @@ function App() {
     <div className="App" style={{overflowX: "hidden" }}>
       <header className="App-header">
         <nav className='navbar fixed-top navbar-expand-lg navbar-dark bg-dark'>
-          <Link to={'/'} className='navbar-brand mx-3'>UWaterloo Concrete Canoe</Link>
+          <Link to={'/'} className='navbar-brand ms-3'><img src={Logo} width="54" height="44" /></Link>
           <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown' aria-controls='navbarNavDropdown' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
