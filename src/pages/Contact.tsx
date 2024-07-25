df9db8aebce6d7aac746190a9e3d27173404f7c4
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-07-24 22:28:33 -0400

    change contact form to custom

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-
 function Contact() {
     
     // to update the embeded form, replace the src="link" in line 11 with the src link for the new embed
 
     return (
-        <div className="Contact" style={{marginTop: "50px"}}>
-            <div className="row justify-content-center text-center" style={{height:"100%"}}>
-                <iframe width="640px" height="630px" title="join-form" src="https://forms.office.com/Pages/ResponsePage.aspx?id=h1o6cprzIkqSRz_CQMATlnO8OjhqoIRIvJhMsNGBHK5UREtBWlYzWlk0WDRINzJLUkdYOEc5NjNGUi4u&embed=true" style={{border: "none", maxWidth: "100%", maxHeight: "100vh"}}> </iframe>
+        <div className="Contact" style={{marginTop: "30px", padding: "10%", alignContent: "center", minHeight: "80vh"}}>
+            <div className="row">
+                <h1>Contact</h1>
+
+                <p>Have a question or want more information? Fill out the form below to get in touch with us!</p>
+            </div>        
+            <div className="row">
+                <form className="col p-3" action="https://formsubmit.co/concretecanoe@uwaterloo.ca" method="POST">
+                    <div className="mb-3">
+                        <label htmlFor="subject" className="form-label">Subject</label>
+                        <input className="row form-control" type="text" id="subject" name="subject" required />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input className="row form-control" type="text" id="name" name="name" required />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input className="row form-control" type="email" id="email" name="email" required />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="message" className="form-label">Please enter your message</label>
+                        <textarea className="row form-control" id="message" name="email" required />
+                    </div>
+                    <button className="row btn btn-primary" type="submit">Send</button>
+                </form> 
             </div>
         </div>
     );
